b231768e49aa6041661bbea9a58bef70b7b43411
Kyle <Kyle@DESKTOP-JCBIVMI>
2017-08-22 19:18:50 -0300

    Add project files.

---
+++ Boticus/MyBot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Boticus
+{
+    class MyBot
+    {
+        DiscordClient discord;
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+            discord.UsingCommands(x =>
+           {
+               x.PrefixChar = '!';
+               x.AllowMentionPrefix = true;
+           });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hi");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzQ5MzE4NDYyNDQ0Nzk3OTYy.DHzwxQ._aWUv9lAzAxw12CnFj2_E0I_Hus", TokenType.Bot);
+            });
+        }
+        private void Log(Object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
