940ed185ae60410d2c37ada84904f5204f1a4817
Marsniper27 <kyle.marshall.2771@gmail.com>
2017-08-22 20:18:42 -0300

    Corrected Case

--- Boticus/MyBot.cs
+++ Boticus/MyBot.cs
@@ -30,7 +30,7 @@ namespace Boticus
             commands.CreateCommand("hello")
                 .Do(async (e) =>
                 {
-                    await e.Channel.SendMessage("which system do you play?");
+                    await e.Channel.SendMessage("Which system do you play?");
                 });
 
             discord.ExecuteAndWait(async () =>
